78f4807d2732301dcdb55bde07ac31b5e38fa83a
Tec. Victor Denley <Airborne82101@outlook.com>
2025-05-19 23:03:09 -0600

    /etc/nixos/packages.nix

---
+++ NIX-OS (Updatable)/packages.nix
@@ -0,0 +1,98 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+let
+  dock-script = pkgs.writeShellScriptBin "dock-sh" ''
+    niri msg output eDP-1 off
+    niri msg output DP-1 vrr on
+  '';
+  undock-script = pkgs.writeShellScriptBin "undock-sh" ''
+    niri msg output eDP-1 on
+    niri msg output eDP-1 vrr on
+  '';
+  firefox-sync = pkgs.writeShellScriptBin "firefox-sync" ''
+        static=static-$1
+        link=$1
+        volatile=/dev/shm/firefox-$1-$USER
+
+        IFS=
+        set -efu
+
+        cd ~/.mozilla/firefox
+
+        if [ ! -r $volatile ]; then
+        	mkdir -m0700 $volatile
+        fi
+
+        if [ "$(readlink $link)" != "$volatile" ]; then
+    	    mv $link $static
+    	    ln -s $volatile $link
+        fi
+
+        if [ -e $link/.unpacked ]; then
+    	    rsync -av --delete --exclude .unpacked ./$link/ ./$static/
+        else
+    	    rsync -av ./$static/ ./$link/
+    	    touch $link/.unpacked
+        fi
+  '';
+in
+{
+  environment.systemPackages = with pkgs; [
+    git
+    niri
+    neovim
+    firefox
+    xfce.thunar
+    xfce.thunar-volman
+    gvfs
+    pipewire
+    libreoffice
+    hunspell
+    hunspellDicts.es_MX
+    hunspellDicts.en_US
+    eww
+    jq
+    jaq
+    swww
+    xwayland-satellite
+    htop
+    pwvucontrol
+    nomacs
+    wl-clipboard
+    qalculate-gtk
+    gnome-themes-extra
+    lazygit
+    grim
+    swaylock
+    bluetuith
+
+    wl-mirror
+    _7zz
+    gnupg
+    pinentry-tty
+    apple-cursor
+    nautilus # Needed for gtk4 FileChooserNative
+    waypaper
+    ripdrag
+    ouch
+
+    dock-script
+    undock-script
+    firefox-sync
+  ];
+  systemd.user.services.firefox-profile-memory-cache = {
+    description = "Firefox profile memory cache";
+    wantedBy = [ "default.target" ];
+    path = [ pkgs.rsync ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      ExecStart = ''${firefox-sync}/bin/firefox-sync 0shu6evv.default'';
+      ExecStop = ''${firefox-sync}/bin/firefox-sync 0shu6evv.default'';
+    };
+  };
+}
