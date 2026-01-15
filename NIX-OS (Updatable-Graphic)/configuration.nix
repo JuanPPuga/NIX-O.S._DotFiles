a0c21c87d4c01aa5fa66ae9f57caa85137aabfb9
Tec. Victor Denley <Airborne82101@outlook.com>
2026-01-15 11:01:51 -0600

    /etc/nixos/configuration.nix [Main Config]

--- NIX-OS (Updatable-Graphic)/configuration.nix
+++ NIX-OS (Updatable-Graphic)/configuration.nix
@@ -115,15 +115,6 @@
     ];
   };
 
-  # Install STEAM.
-  nixpkgs.config.allowUnfree = true;
-  programs.steam.enable = true;
-  programs.steam.gamescopeSession.enable = true;
-  programs.gamemode.enable = true;
-
-  # Install firefox.
-  programs.firefox.enable = true;
-
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
