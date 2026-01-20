528f790a5761102133087572a3ed508def0a5cce
Tec. Victor Denley <Airborne82101@outlook.com>
2026-01-20 00:29:54 -0600

    /etc/nixos/printers-configuration.nix [Printers: WIFI/USB]

--- NIX-OS (Updatable-Graphic)/printers-configuration.nix
+++ NIX-OS (Updatable-Graphic)/printers-configuration.nix
@@ -1,22 +1,32 @@
 { config, pkgs, ... }:
 
 {
-  #Printer-Services: HP.
+  # Printer Services: HP
   services.printing = {
     enable = true;
-    drivers = [
-      pkgs.hplip
-    ];
+    drivers = [ pkgs.hplip ];
   };
 
-  #Auto detect WI-FI Printers (Local).  
+  # Auto detect WI-FI Printers (Local) – opcional
   services.avahi = {
     enable = true;
     nssmdns4 = true;
   };
 
-  # For USB port - Printers.
-  hardware.printers = {
-    ensurePrinters = [];
+  # Scanner (HP Ink Tank 315 – USB)
+  hardware.sane = {
+    enable = true;
+    extraBackends = [ pkgs.hplipWithPlugin ];
   };
+
+  # UDEV for HP USB devices
+  services.udev.packages = [ pkgs.hplip ];
+
+  # User permissions
+  users.users.user_933.extraGroups = [ "scanner" "lp" ];
+
+  # Configuración SANE: usar solo el backend HP (hpaio)
+  environment.etc."sane.d/dll.conf".text = ''
+    hpaio
+  '';
 }
